b1c432f84f1fcd4a205e8c4865a18a550145b846
Dominika Strečanská <115701063+nikkyjetop@users.noreply.github.com>
2023-08-25 00:28:46 +0200

    Update cv_4.m

--- SEM2/cv4/cv_4.m
+++ SEM2/cv4/cv_4.m
@@ -1,23 +1,24 @@
-% Vytvoreni matice M
-M = [0, 1, 1, 1, 0; 
+% Generování matice M
+M = [0, 1, 1, 1, 1; 
      1, 0, 1, 0, 0; 
-     1, 1, 0, 1, 1; 
-     1, 0, 0, 0, 0; 
-     1, 0, 1, 1, 0];
+     1, 1, 0, 0, 1; 
+     1, 0, 1, 0, 1; 
+     0, 0, 1, 0, 0];
+
+% Normalizace matice M
+for i = 1:size(M, 1)
+    M(:, i) = M(:, i) / sum(M(:, i));
+end
 
 % Počáteční vektor y0
 y0 = ones(size(M, 1), 1) / size(M, 1);
 
-% Tolerance a počet iterací
-tol = 1e-6;
+% Spuštění mocninové metody
 maxit = 1000;
-
-% Spusteni mocninove metody
+tol = 1e-7;
 [vec, lambda] = mocninna_metoda(M, y0, tol, maxit);
 
-% Vytvoreni seznamu webovych stranek
-web_stranky = {'A', 'B', 'C', 'D', 'E'};
-
-% Razeni stranek podle PageRank
-[~, index] = sort(vec, 'descend');
-webs = web_stranky(index);
+% Seřazení stránek podle PageRank
+stranky = ['A', 'B', 'C', 'D', 'E'];
+[~, indexy] = sort(vec);
+webs = stranky(indexy);
