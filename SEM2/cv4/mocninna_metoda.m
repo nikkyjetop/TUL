67448838ecda4ae5a42f4fce6379db6e36d5133f
Dominika Strečanská <115701063+nikkyjetop@users.noreply.github.com>
2023-08-25 00:27:55 +0200

    Update mocninna_metoda.m

--- SEM2/cv4/mocninna_metoda.m
+++ SEM2/cv4/mocninna_metoda.m
@@ -1,26 +1,21 @@
 function [vec, lambda] = mocninna_metoda(A, y0, tol, maxit)
-    % Inicializace proměnných
-    n = length(y0);
-    vec = y0;
-    lambda = 0;
-
-    % Iterace
-    for it = 1:maxit
-        % Mocnina matice A na vektor vec
-        vec = A * vec;
-
-        % Normalizace vektoru
-        vec = vec / norm(vec);
-
-        % Výpočet vlastního čísla
-        lambda_new = vec' * A * vec;
-
-        % Kontrola konvergence
-        if abs(lambda_new - lambda) < tol
-            lambda = lambda_new;
+    for j = 1:maxit
+        u = y0 / norm(y0);
+        y0 = A * u;
+        lambda = u' * y0;
+        vec = y0 / norm(y0);
+        if norm(lambda * u - y0) < tol
             break;
         end
-
-        lambda = lambda_new;
     end
 end
+
+% u je počáteční vektor, který inicializujeme na y0 normovaný na  délku y0. 
+% Získame tím vektor s délkou 1
+% iterace se opakuje maxit krát nebo dokud není dosaženo tolerance
+% pomocí vynásobení u maticí A dostaneme vektor y0 a aktualizujeme odhad vlastního vektoru
+% lambda je vypočteno jako skalární součin mezi původním vektorem u a novým vektorem y0. 
+% lambda představuje aproximaci dominantního vlastního čísla matica A
+% kontrola konvergence - kontrolujeme zda se aproximace dominantního 
+% vlastního čísla lambda zmenšila o méně než tol mezi iteracemi. 
+% Pokud ano, algoritmus se ukončí
